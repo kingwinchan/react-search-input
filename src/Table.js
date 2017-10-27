345b24b7ebbeea5f388669971180cc2b365e7140
kingwinchan <kingwinchan@126.com>
2017-10-27 19:37:01 +1100

    add style for mobile side by using react-strap.

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap'; 
 
 function isSearched(searchTerm) {
     return function (item) {
@@ -11,19 +12,23 @@ const Table = ({ list, pattern }) =>
     <div className="table">
         {list.records.filter(isSearched(pattern)).map(item =>
             <div key={item.objectID} className="table-row">
-                <span style={{ width: '40%' }}>
-                    <i className="iconfont">&#xe684;</i>
-                    &nbsp;{item.name}
-                </span>
-                <span style={{ width: '40%' }}>
-                    {item.email}
-                </span>
-                <span style={{ width: '20%' }}>
-                    {item.time}
-                </span>
-                <span style={{ width: '10%' }}>
-                    <i className="iconfont">&#xe600;</i>
-                </span>
+                <Container fluid="true">
+                    <Row className="show-grid">
+                        <Col xs="12" sm="6" md="4">
+                            <i className="iconfont">&#xe684;</i>
+                            &nbsp;{item.name}
+                        </Col>
+                        <Col xs="12" sm="6" md="4">
+                            {item.email}
+                        </Col>
+                        <Col xs="12" sm="6" md="3">
+                            {item.time}
+                        </Col>
+                        <Col xs="12" sm="6" md="1">
+                            <i className="iconfont">&#xe600;</i>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )}
     </div>
